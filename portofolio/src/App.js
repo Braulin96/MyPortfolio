223bdb52f201c76a21e8410ab01d433946a20928
Braulin Pires <braulinpires@hotmail.com>
2024-01-23 11:39:57 +0000

    added a Navbar component in App

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,8 +1,10 @@
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <HeroSection/>
     </div>
   );
