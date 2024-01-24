8578f6a3bfd70fd024a0546e4fffc0eb1006ac5d
Braulin Pires <braulinpires@hotmail.com>
2024-01-24 09:45:40 +0000

    Use of Absolute Paths Instead of Relative Paths in App

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,5 +1,6 @@
-import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
+import HeroSection from "components/HeroSection";
+import Navbar from "components/Navbar";
+
 
 function App() {
   return (
