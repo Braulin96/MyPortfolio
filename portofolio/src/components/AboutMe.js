3c7f4baf86c7b1f1d41e07eae4b3fd39eaabde33
Braulin Pires <braulinpires@hotmail.com>
2024-01-24 10:36:25 +0000

    created AboutMe section

---
+++ portofolio/src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AboutMe() {
+  return (
+    <div className="text-red-500 md:h-[850px] h-[650px]">
+    <div className="max-w-7xl h-full flex items-center justify-center mx-auto ">
+        <p className='text-8xl font-semibold text-[#BDD1C5] opacity-60 leading-tight'>About<br/>Me</p>
+        <p className='text-4xl text-[#4F5853] font-extralight -ml-36 mt-12 z-20'>A front-End Developer who love creating websites that are <br/>
+         not only visually appealing but also <scan className="font-normal">user-friendly</scan> .</p>
+    </div>
+    </div>
+  )
+}
+
+export default AboutMe
