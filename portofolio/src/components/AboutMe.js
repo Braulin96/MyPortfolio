a90d272411b4149d55afd40b1049d031fa30cbcf
Braulin Pires <braulinpires@hotmail.com>
2024-01-24 11:04:53 +0000

    set Responsiviness to AboutMe section

--- portofolio/src/components/AboutMe.js
+++ portofolio/src/components/AboutMe.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function AboutMe() {
   return (
-    <div className="text-red-500 md:h-[850px] h-[650px]">
-    <div className="max-w-7xl h-full flex items-center justify-center mx-auto ">
-        <p className='text-8xl font-semibold text-[#BDD1C5] opacity-60 leading-tight'>About<br/>Me</p>
-        <p className='text-4xl text-[#4F5853] font-extralight -ml-36 mt-12 z-20'>A front-End Developer who love creating websites that are <br/>
-         not only visually appealing but also <scan className="font-normal">user-friendly</scan> .</p>
+    <div className="text-red-500" style={{ height: "100vh" }}>
+      <div className="max-w-7xl h-full flex items-center justify-center mx-auto ">
+        <div className="lg:flex items-center justify-center mx-auto px-4">
+          <p className="sm:text-8xl text-6xl font-semibold text-[#BDD1C5] opacity-50 lg:leading-tight leading-none">
+            About
+            <br />
+            Me
+          </p>
+          <p className="sm:text-4xl text-2xl text-[#4F5853] font-extralight lg:-ml-36 lg:mt-12 mt-4 z-20">
+            A front-End Developer who loves creating websites that are not only
+            visually appealing but also
+            <span className="font-normal ml-2">user-friendly</span>.
+          </p>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
