24b81240653833b7eb5febf17638bcb825b609ad
Braulin Pires <braulinpires@hotmail.com>
2024-01-24 12:28:52 +0000

    added MyProjects component

---
+++ portofolio/src/components/MyProjects.js
@@ -0,0 +1,21 @@
+const MyProjects = () => {
+  return (
+    <div className="text-red-500" style={{ height: "100vh" }}>
+      <div className="max-w-7xl h-full flex items-center justify-center mx-auto ">
+        <div className="lg:flex items-center justify-center mx-auto px-4">
+          <p className="sm:text-8xl text-6xl font-semibold text-[#BDD1C5] opacity-50 lg:leading-tight leading-none order-last">
+            My
+            <br />
+            Projects
+          </p>
+          <p className="sm:text-4xl text-2xl text-[#4F5853] font-extralight z-20 lg:mt-12 mt-6">
+            Focused in turn ideas into visually striking websites with a focus
+            on exceptional
+            <span className="font-normal ml-2">user experiences</span>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MyProjects;
