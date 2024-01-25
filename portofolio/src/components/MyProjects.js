a58a9d88461a9f373cbfd0478bd442a256263b86
Braulin Pires <braulinpires@hotmail.com>
2024-01-25 21:50:14 +0000

    removed empty spaces

--- portofolio/src/components/MyProjects.js
+++ portofolio/src/components/MyProjects.js
@@ -1,6 +1,6 @@
 const MyProjects = () => {
   return (
-    <div className="text-red-500" style={{ height: "100vh" }}>
+    <div style={{ height: "100vh" }}>
       <div className="max-w-7xl h-full flex items-center justify-center mx-auto ">
         <div className="lg:flex items-center justify-center mx-auto px-4">
           <p className="sm:text-8xl text-6xl font-semibold text-[#BDD1C5] opacity-50 lg:leading-tight leading-none order-last">
