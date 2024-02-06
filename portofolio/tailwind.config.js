73c2c8af9e4ec3fca06bcad12e1d38bf46b80255
Braulin Pires <braulinpires@hotmail.com>
2024-02-06 12:18:33 +0000

    set new custom colours in tailwind file

--- portofolio/tailwind.config.js
+++ portofolio/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#00CD8F',
+        'secondary-gray':'#D3D6E0',
+        'secondary-blue' : '#1D1B27'
+      }
+    },
   },
   plugins: [
     require('tailwindcss-animated')
