54737b8f095694eb463da15aeef365bc6f00b24e
Braulin Pires <braulinpires@hotmail.com>
2024-02-06 20:56:49 +0000

    created a new Gsap Animation for testing

---
+++ portofolio/src/components/shared/GsapAnimation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import LaptopTWD from "assets/images/laptopTWD.png";
+
+const GsapAnimation = () => {
+  return (
+    <div className="main">
+      <div className="container">
+        <>
+          <div className="img-container">
+            <img src={LaptopTWD} />
+          </div>
+        </>
+      </div>
+    </div>
+  );
+};
+export default GsapAnimation;
