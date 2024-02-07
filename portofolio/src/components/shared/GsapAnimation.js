6a81abf8cf5c9778ad5214c800d2a51bbbd28012
Braulin Pires <braulinpires@hotmail.com>
2024-02-07 12:16:58 +0000

    clean GsapAnimation

--- portofolio/src/components/shared/GsapAnimation.js
+++ portofolio/src/components/shared/GsapAnimation.js
@@ -1,5 +1,4 @@
 import React, { useRef, useEffect } from "react";
-import LaptopTWD from "assets/images/laptopTWD.png";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import CSSRulePlugin from "gsap/CSSRulePlugin";
@@ -12,44 +11,33 @@ const GsapAnimation = ({ children }) => {
   let container = useRef(null);
   let imageReveal = CSSRulePlugin.getRule(".img-container:after");
 
-  let tl = gsap.timeline({
-    paused: true, // Pause the timeline initially
-  });
-
   useEffect(() => {
-    tl.to(container, 0.2, { css: { visibility: "visible" } });
+    // Create a local timeline for each component instance
+    let tl = gsap.timeline();
+    tl.to(container.current, 0.2, { css: { visibility: "visible" } });
     tl.to(imageReveal, 1.4, { width: "0%", ease: "power2.inOut" });
-    // tl.from(image, {
-    //   scale: 1.4, // Escala inicial de 1.4
-    //   ease: "power2.inOut",
-    //   //duration: 1.4, // Duração da animação
-    //   delay: 1.4, // Atraso antes do início da animação
-    // });
-    // // Adicionando uma etapa para restaurar a escala original após a animação
-    // tl.to(image, { scale: 1, duration: 0.01 }); // Define a escala do elemento image para 1 (escala original)
 
-    // Create a ScrollTrigger for the section
+    // Create a local ScrollTrigger for each component instance
     ScrollTrigger.create({
-      trigger: container,
+      trigger: container.current,
       start: "top 70%", // Adjust the start position as needed
       onEnter: () => tl.play(), // Play the timeline when the section is entered
+      onLeaveBack: () => tl.reverse(), // Reverse the timeline when leaving the section
     });
-    //tl.set(image, { scale: 1 });
 
     // Cleanup
     return () => {
+      tl.kill();
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
-  }, [tl, container, image, imageReveal]); // Add tl, container, image, and imageReveal to dependency array
+  }, [container, imageReveal]);
 
   return (
     <section className="main">
-      <div className="container" ref={(el) => (container = el)}>
+      <div className="container" ref={container}>
         <div className="img-container">
           <div
-            ref={(el) => {
-              image = el;
-            }}
+            ref={image}
           />
           {children}
         </div>
