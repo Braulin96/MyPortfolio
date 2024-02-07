b5f1957c352b49bdc585eb13cf41740948a43c56
Braulin Pires <braulinpires@hotmail.com>
2024-02-07 00:11:26 +0000

    use the animation for div instead of img

--- portofolio/src/components/shared/GsapAnimation.js
+++ portofolio/src/components/shared/GsapAnimation.js
@@ -3,53 +3,55 @@ import LaptopTWD from "assets/images/laptopTWD.png";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import CSSRulePlugin from "gsap/CSSRulePlugin";
-import './GsapAnimation.scss';
+import "./GsapAnimation.scss";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const GsapAnimation = () => {
+const GsapAnimation = ({ children }) => {
   let image = useRef(null);
   let container = useRef(null);
   let imageReveal = CSSRulePlugin.getRule(".img-container:after");
 
   let tl = gsap.timeline({
-    paused: true // Pause the timeline initially
+    paused: true, // Pause the timeline initially
   });
 
   useEffect(() => {
-    tl.to(container, 0, { css: { visibility: "visible" } });
+    tl.to(container, 0.2, { css: { visibility: "visible" } });
     tl.to(imageReveal, 1.4, { width: "0%", ease: "power2.inOut" });
-    tl.from(image, 1.4, {
-      scale: 1.6,
-      ease: "power2.inOut",
-      delay: -1.4
-    });
+    // tl.from(image, {
+    //   scale: 1.4, // Escala inicial de 1.4
+    //   ease: "power2.inOut",
+    //   //duration: 1.4, // Duração da animação
+    //   delay: 1.4, // Atraso antes do início da animação
+    // });
+    // // Adicionando uma etapa para restaurar a escala original após a animação
+    // tl.to(image, { scale: 1, duration: 0.01 }); // Define a escala do elemento image para 1 (escala original)
 
     // Create a ScrollTrigger for the section
     ScrollTrigger.create({
       trigger: container,
-      start: "top 80%", // Adjust the start position as needed
-      onEnter: () => tl.play() // Play the timeline when the section is entered
+      start: "top 70%", // Adjust the start position as needed
+      onEnter: () => tl.play(), // Play the timeline when the section is entered
     });
+    //tl.set(image, { scale: 1 });
 
     // Cleanup
     return () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
   }, [tl, container, image, imageReveal]); // Add tl, container, image, and imageReveal to dependency array
 
   return (
     <section className="main">
-      <p>GSAP IMAGE REVEAL</p>
-      <div className="container" ref={el => (container = el)}>
+      <div className="container" ref={(el) => (container = el)}>
         <div className="img-container">
-          <img
-            ref={el => {
+          <div
+            ref={(el) => {
               image = el;
             }}
-            src={LaptopTWD}
-            alt="Laptop"
           />
+          {children}
         </div>
       </div>
     </section>
