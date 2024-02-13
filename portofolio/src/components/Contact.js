018a793af1c56ee5071e84243a6d91231ad0ed50
Braulin Pires <braulinpires@hotmail.com>
2024-02-13 11:45:40 +0000

    change the colors of text in Contact component

--- portofolio/src/components/Contact.js
+++ portofolio/src/components/Contact.js
@@ -7,17 +7,17 @@ import { AiFillInstagram } from "react-icons/ai";
 const Contact = () => {
   return (
     <div style={{ height: "100vh", maxHeight:'1000px'}}>
-    <div className="max-w-7xl h-full flex items-center justify-center mx-auto px-4 bg-gradient-to-b from-white via-white to-[#e1eae4]"
+    <div className="max-w-7xl h-full flex items-center justify-center mx-auto px-4"
       >
       <div className="flex flex-col items-center justify-center mx-auto px-4 gap-y-4 lg:gap-x-8 w-full">
-        <p className="sm:text-8xl text-6xl font-semibold text-[#BDD1C5] opacity-50 w-full sm:text-center lg:ml-56">
+        <p className="lg:text-8xl md:text-7xl text-6xl font-semibold text-white w-full sm:text-center lg:ml-56">
          Contact
         </p>
         <div className="flex sm:gap-x-14 gap-x-10 lg:mr-56 w-full sm:w-auto">
-            <FaGithub size={45} color="#4F5853"/>
-            <IoIosMail size={45} color="#4F5853"/>
-            <FaLinkedinIn size={45} color="#4F5853"/>
-            <AiFillInstagram size={45} color="#4F5853"/>
+            <FaGithub size={45} color="#EEEEEE"/>
+            <IoIosMail size={45} color="#EEEEEE"/>
+            <FaLinkedinIn size={45} color="#EEEEEE"/>
+            <AiFillInstagram size={45} color="#EEEEEE"/>
         </div>
       </div>
     </div>
