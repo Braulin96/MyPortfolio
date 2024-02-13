c1914e60656e3d89a5b8283628c27cf06e7861c0
Braulin Pires <braulinpires@hotmail.com>
2024-02-13 13:45:53 +0000

    added a lottie file player

--- portofolio/src/components/Contact.js
+++ portofolio/src/components/Contact.js
@@ -1,25 +1,33 @@
-//Note : Icon:
+import Lottie from "lottie-react";
+//Note : Icons:
 import { FaGithub } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { FaLinkedinIn } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
+import Animation from '../assets/images/contactAnimation.json'
+import Second from '../assets/images/second.json'
 
 const Contact = () => {
   return (
-    <div style={{ height: "100vh", maxHeight:'1000px'}}>
-    <div className="max-w-7xl h-full flex items-center justify-center mx-auto px-4"
+    <div className="" style={{ height: "100vh", maxHeight:'1000px'}}>
+    <div className="max-w-7xl h-full flex items-center mx-auto px-4"
       >
-      <div className="flex flex-col items-center justify-center mx-auto px-4 gap-y-4 lg:gap-x-8 w-full">
-        <p className="lg:text-8xl md:text-7xl text-6xl font-semibold text-white w-full sm:text-center lg:ml-56">
+      <div className="flex flex-col items-center px-4 gap-y-12 w-full">
+        <p className="lg:text-8xl md:text-7xl text-6xl font-semibold text-white w-full">
          Contact
         </p>
-        <div className="flex sm:gap-x-14 gap-x-10 lg:mr-56 w-full sm:w-auto">
-            <FaGithub size={45} color="#EEEEEE"/>
-            <IoIosMail size={45} color="#EEEEEE"/>
-            <FaLinkedinIn size={45} color="#EEEEEE"/>
-            <AiFillInstagram size={45} color="#EEEEEE"/>
+        <div className="flex sm:gap-x-20 gap-x-10 w-full ml-36">
+            <FaGithub size={50} color="#EEEEEE"/>
+            <IoIosMail size={50} color="#EEEEEE"/>
+            <FaLinkedinIn size={50} color="#EEEEEE"/>
+            <AiFillInstagram size={50} color="#EEEEEE"/>
         </div>
       </div>
+      <div className="flex my-auto">
+      <Lottie  className="my-auto opacity-40" animationData={Second} loop={true}   style={{ width: "500px" }} />
+
+      </div>
+    
     </div>
   </div>
   )
