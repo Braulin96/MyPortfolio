7a82e089f6ec3241b6da504ab83d4ceadfca866f
Braulin Pires <braulinpires@hotmail.com>
2024-02-14 13:22:30 +0000

    wrap the FadeOnScroll animation to each ul list with diferent delay

--- portofolio/src/components/Skills.js
+++ portofolio/src/components/Skills.js
@@ -10,34 +10,42 @@ const Skills = () => {
             </p>
           </FadeOnScroll>
           <div className="w-full">
-            <FadeOnScroll data="fade" delay="500" duration="1000">
+            
               <div className="grid md:grid-cols-4 grid-cols-2  gap-x-10 md:text-xl text-base lg:mt-0 mt-6 gap-y-12 text-gray-200">
+              <FadeOnScroll data="fade" delay="500" duration="1000">
                 <ul className="font-light flex flex-col gap-y-4 mx-auto w-40">
                   <li>React JS</li>
                   <li>JavaScript</li>
                   <li>Html & CSS</li>
                   <li>TypeScript</li>
                 </ul>
+                </FadeOnScroll>
+                <FadeOnScroll data="fade" delay="700" duration="1000">
                 <ul className="font-light flex flex-col gap-y-4 lg:mt-16 mx-auto w-40">
                   <li>Resilience</li>
                   <li>Problem-solving</li>
                   <li>Analytical Abilities</li>
                   <li>Adaptability</li>
                 </ul>
+                </FadeOnScroll>
+                <FadeOnScroll data="fade" delay="900" duration="1000">
                 <ul className="font-light flex flex-col gap-y-4 mx-auto w-40">
                   <li>Responsiveness</li>
                   <li>NPM</li>
                   <li>Git/Git Hub</li>
                   <li>Figma</li>
                 </ul>
+                </FadeOnScroll>
+                <FadeOnScroll data="fade" delay="1100" duration="1000">
                 <ul className="font-light flex flex-col gap-y-4 lg:mt-16 mx-auto w-40">
                   <li>TeamWork</li>
                   <li>Continuous Learning</li>
                   <li>Project Management</li>
                   <li>Creativity</li>
                 </ul>
+                </FadeOnScroll>
               </div>
-            </FadeOnScroll>
+        
           </div>w
         </div>
       </div>
