2b656f2410d662686486863c19149a7c7e95b246
Braulin Pires <braulinpires@hotmail.com>
2024-02-14 22:39:48 +0000

    use new robot animation lottie file and set new timing for FadeOnScroll in Contact section

--- portofolio/src/components/Contact.js
+++ portofolio/src/components/Contact.js
@@ -6,52 +6,44 @@ import { FaGithub } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { FaLinkedinIn } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
-import Animation from "../assets/images/contactAnimation.json";
-import Second from "../assets/images/second.json";
-import Mobile from "../assets/images/mobileAnimeSecond.json";
+import Robot from "../assets/images/robot.json";
 
 const Contact = () => {
   return (
-    <div
-     
-      style={{ height: "100vh", maxHeight: "1000px" }}
-    >
+    <div style={{ height: "100vh", maxHeight: "1000px" }}>
       <div className="max-w-7xl my-auto h-full flex sm:flex-row flex-col items-center mx-auto">
-        
         <div className="flex flex-col px-4 gap-y-12 w-full pt-20 sm:py-0">
           <FadeOnScroll data="fade" duration="1000" delay="0">
-          <p className="lg:text-8xl md:text-7xl text-6xl font-semibold text-white w-full">
-            Contact
-          </p>
+            <p className="lg:text-8xl md:text-7xl text-6xl font-semibold text-white w-full">
+              Contact
+            </p>
           </FadeOnScroll>
-         
+
           <div className="flex sm:gap-x-20 gap-x-10 sm:justify-center">
-          <FadeOnScroll data="fade" duration="1000" delay="500">
-            <FaGithub size={50} color="#EEEEEE" />
+            <FadeOnScroll data="fade" duration="1000" delay="500">
+              <FaGithub size={50} color="#EEEEEE" />
             </FadeOnScroll>
             <FadeOnScroll data="fade" duration="1000" delay="700">
-            <IoIosMail size={50} color="#EEEEEE" />
+              <IoIosMail size={50} color="#EEEEEE" />
             </FadeOnScroll>
             <FadeOnScroll data="fade" duration="1000" delay="900">
-            <FaLinkedinIn size={50} color="#EEEEEE" />
+              <FaLinkedinIn size={50} color="#EEEEEE" />
             </FadeOnScroll>
             <FadeOnScroll data="fade" duration="1000" delay="1200">
-            <AiFillInstagram size={50} color="#EEEEEE" />
+              <AiFillInstagram size={50} color="#EEEEEE" />
             </FadeOnScroll>
           </div>
-          
         </div>
-        
-        <div className="flex my-auto sm:mr-12">
-        <FadeOnScroll data="fade" duration="1200" delay="1600" offset="400">
-          <Lottie
-            className="my-auto opacity-80"
-            animationData={Mobile}
-            loop={true}
-            //style={{ maxWidth: "800px" }}
-          />
-          </FadeOnScroll>
-        </div> 
+        <FadeOnScroll data="fade" duration="1600" delay="1500" offset="400">
+          <div className="flex my-auto sm:mr-12 bg-white  bg-opacity-10 rounded-full p-5 shadow-md sm:mx-0 mx-4">
+            <Lottie
+              className="my-auto opacity-80"
+              animationData={Robot}
+              loop={true}
+              style={{ maxWidth: "600px" }}
+            />
+          </div>
+        </FadeOnScroll>
       </div>
     </div>
   );
