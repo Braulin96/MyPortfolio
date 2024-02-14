790a4d36ebd83cd3907c016bc593bdd7e270f569
Braulin Pires <braulinpires@hotmail.com>
2024-02-14 13:01:51 +0000

    added the FadeOnScrooll animation to Skills component

--- portofolio/src/components/Skills.js
+++ portofolio/src/components/Skills.js
@@ -1,38 +1,44 @@
+import FadeOnScroll from "./shared/FadeOnScroll";
 const Skills = () => {
   return (
-    <div style={{ height: "100vh", maxHeight:'1000px'}}>
+    <div style={{ height: "100vh", maxHeight: "1000px" }}>
       <div className="max-w-7xl h-full flex mx-auto">
         <div className="lg:flex items-center w-full px-4 justify-center my-auto">
-          <p className="sm:text-8xl text-6xl font-semibold text-primary-gray">
-            Skills
-          </p>
-          <div className="grid md:grid-cols-4 grid-cols-2 w-full gap-x-10 md:text-xl text-base lg:mt-0 mt-6 gap-y-12 text-gray-200">
-            <ul className="font-light flex flex-col gap-y-4 mx-auto w-40">
-              <li>React JS</li>
-              <li>JavaScript</li>
-              <li>Html & CSS</li>
-              <li>TypeScript</li>
-            </ul>
-            <ul className="font-light flex flex-col gap-y-4 lg:mt-16 mx-auto w-40">
-              <li>Resilience</li>
-              <li>Problem-solving</li>
-              <li>Analytical Abilities</li>
-              <li>Adaptability</li>
-            </ul>
-
-            <ul className="font-light flex flex-col gap-y-4 mx-auto w-40">
-              <li>Responsiveness</li>
-              <li>NPM</li>
-              <li>Git/Git Hub</li>
-              <li>Figma</li>
-            </ul>
-            <ul className="font-light flex flex-col gap-y-4 lg:mt-16 mx-auto w-40">
-              <li>TeamWork</li>
-              <li>Continuous Learning</li>
-              <li>Project Management</li>
-              <li>Creativity</li>
-            </ul>
-          </div>
+          <FadeOnScroll data="fade" delay="0" duration="1000">
+            <p className="sm:text-8xl text-6xl font-semibold text-primary-gray">
+              Skills
+            </p>
+          </FadeOnScroll>
+          <div className="w-full">
+            <FadeOnScroll data="fade" delay="500" duration="1000">
+              <div className="grid md:grid-cols-4 grid-cols-2  gap-x-10 md:text-xl text-base lg:mt-0 mt-6 gap-y-12 text-gray-200">
+                <ul className="font-light flex flex-col gap-y-4 mx-auto w-40">
+                  <li>React JS</li>
+                  <li>JavaScript</li>
+                  <li>Html & CSS</li>
+                  <li>TypeScript</li>
+                </ul>
+                <ul className="font-light flex flex-col gap-y-4 lg:mt-16 mx-auto w-40">
+                  <li>Resilience</li>
+                  <li>Problem-solving</li>
+                  <li>Analytical Abilities</li>
+                  <li>Adaptability</li>
+                </ul>
+                <ul className="font-light flex flex-col gap-y-4 mx-auto w-40">
+                  <li>Responsiveness</li>
+                  <li>NPM</li>
+                  <li>Git/Git Hub</li>
+                  <li>Figma</li>
+                </ul>
+                <ul className="font-light flex flex-col gap-y-4 lg:mt-16 mx-auto w-40">
+                  <li>TeamWork</li>
+                  <li>Continuous Learning</li>
+                  <li>Project Management</li>
+                  <li>Creativity</li>
+                </ul>
+              </div>
+            </FadeOnScroll>
+          </div>w
         </div>
       </div>
     </div>
