bae199ded1f5e590d5e9c567a485ab25836a18a6
Braulin Pires <braulinpires@hotmail.com>
2024-02-19 14:55:23 +0000

    set mr on larger screens between the texts in AboutMe and MyProjects section

--- portofolio/src/components/MyProjects.js
+++ portofolio/src/components/MyProjects.js
@@ -8,16 +8,15 @@ const MyProjects = () => {
         <div className="lg:flex items-center justify-center mx-auto px-4">
           <div className="order-last">
             <FadeOnScroll data="fade" duration="1000" delay="0">
-              <p className="sm:text-8xl text-6xl font-semibold text-secondary-gray lg:leading-tight leading-none ">
+              <p className="sm:text-8xl text-6xl font-semibold text-secondary-gray lg:leading-tight leading-none">
                 My
                 <br />
                 Projects
               </p>
             </FadeOnScroll>
           </div>
-
           <FadeOnScroll data="fade" duration="1000" delay="500">
-          <p className="sm:text-4xl text-2xl text-gray-200 font-extralight z-20 lg:mt-12 mt-6">
+          <p className="sm:text-4xl text-2xl text-gray-200 font-extralight z-20 lg:mt-12 mt-6 lg:mr-4">
             Focused in turn ideas into visually striking websites with a focus
             on exceptional
             <span className="font-normal ml-2 text-primary-green">
